c204452237106b0afcfee2685168753d90c97723
ghetehri <gregoryh2000@gmail.com>
2020-01-25 15:22:59 -0500

    basic UI, DB setup and Enviroment variables

--- client/src/Components/APITest.js
+++ client/src/Components/APITest.js
@@ -1,24 +1,56 @@
 import React, { Component } from 'react';
 import { Button } from 'react-bootstrap';
+import PieChart from 'react-minimal-pie-chart';
 
 export default class APITest extends Component {
     constructor(props) {
         super(props);
-        this.state = { apiResponse: "" };
+        this.state = { garbageCans: [] };
     }
     
     callAPI() {
-        fetch("http://localhost:9000/testAPI")
+        fetch("http://localhost:9000/database")
             .then(res => res.text())
-            .then(res => this.setState({ apiResponse: res }));
+            .then(res => this.setState({ garbageCans: JSON.parse(res) }));
+    }
+    
+    componentWillMount() {
+        this.callAPI();
     }
 
     render() {
-        let {apiResponse} = this.state;
+        const {garbageCans} = this.state;
+
         return (
         <div>
-            <Button className="btn btn-info" onClick={() => this.callAPI()}>Hit API</Button>
-            <p>API returns: {apiResponse}</p>
+            {/* <Button className="btn btn-info" onClick={() => this.callAPI()}>Hit API</Button> */}
+            <p>API returns:</p>
+            {garbageCans.length > 0 && garbageCans.map(garbageCan => 
+                <div>
+                    <p key={garbageCan._id}>Name: {garbageCan._name}</p>
+                    <PieChart
+                        data={[{ value: 1, key: 1, color: '#E38627' }]}
+                        reveal={garbageCan._percentFull*100}
+                        lineWidth={15}
+                        radius={40}
+                        data={[
+                            {
+                              color: '#E38627',
+                              value: garbageCan._percentFull*100 
+                            }
+                          ]}
+                        label
+                        labelPosition={0}
+                        labelStyle={{
+                            fontFamily: 'sans-serif',
+                            fontSize: '25px'
+                        }}
+                        background="#bfbfbf"
+                        animate
+                    />
+                </div>
+            )}
+            
         </div>
         );
     }
